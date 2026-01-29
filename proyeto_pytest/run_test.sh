cb7d0e03e5a75350fad125ed5357b57782053965
Oriana <aleja061213@gmail.com>
2026-01-29 03:47:42 -0300

    corrigiendo

--- proyeto_pytest/run_test.sh
+++ proyeto_pytest/run_test.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-echo "activando el entorno virtual"
+echo "Iniciando ejecuciòn de pruebas Jenkins..."
+
+# Verificar si el entorno virtual existe
 if [ ! -d "venv" ]; then
-    echo "creando el entorno virtual"
+    echo "Entorno virtual no encontrado. Creandolo..."
     python3 -m venv venv
 fi
-source venv/bin/activate
 
+# Activar el entorno virtual correctamente
+echo "activando el entorno virtual"
+if [ -f "venv/bin/activate" ]; then
+    source venv/bin/activate
+elif [ -f "venv/bin/activate" ]; then
+    source venv/bin/activate
+else
+    echo "No se pudo activar el entorno virtual"
+    exit 1
+fi
+
+# Verificar si pip esta instalado correctamente
 echo "instalando independencias"
-pip install --upgrade pip
-pip install -r requirements.txt
+pip install --upgrade pip --break-system-packages
+pip install -r requirements.txt --break-system-packages
 
 echo "ejecutando pruebas con pytest"
 pytest test/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
