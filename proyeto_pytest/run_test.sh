6bbff6df9764fbe523bbaa0e8b9a8250af4c9a11
Oriana <aleja061213@gmail.com>
2026-01-29 03:36:43 -0300

    entorno virtual

--- proyeto_pytest/run_test.sh
+++ proyeto_pytest/run_test.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 echo "activando el entorno virtual"
+if [ ! -d "venv" ]; then
+    echo "creando el entorno virtual"
+    python3 -m venv venv
+fi
 source venv/bin/activate
 
 echo "instalando independencias"
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "ejecutando pruebas con pytest"
